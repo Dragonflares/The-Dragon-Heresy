966dcc443fb4deaf96cfba662e94f3554b9d2c02
Dragonflares <ivanlose@hotmail.com>
2019-09-23 19:50:09 -0300

    Create DragonflaresBot
    Salutations!

---
+++ Dragonflares_Bot.js
@@ -0,0 +1,67 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.on('ready', () => {
+    console.log("I need a new job, yet I logged as " + client.user.tag)
+
+    client.user.setActivity("Space Engineers")
+
+    client.guilds.forEach((guild) => {
+        console.log(guild.name)
+        guild.channels.forEach((channel) => {
+            console.log(' - ', channel.name, channel.id)
+        })
+    })
+    // general de TBG 436263609711067151
+    let generalChannel = client.channels.get("436263609711067151")
+    //generalChannel.send("Buenas noches la concha de su madre")
+})
+
+client.on('message', (receivedMessage) => {
+    if(receivedMessage.author == client.user) {
+        return
+    }
+    //receivedMessage.channel.send("Y vos sos un pelotudo " 
+    //   + receivedMessage.author)
+
+    //receivedMessage.react("👍")
+
+    if(receivedMessage.content.startsWith("#")){
+        processCommand(receivedMessage)
+    }
+})
+
+function processCommand(receivedMessage){
+    let fullCommand = receivedMessage.content.substr(1)
+    let splitCommand = fullCommand.split(" ")
+    let primaryCommand = splitCommand[0]
+    let arguments = splitCommand.slice(1)
+
+    if(primaryCommand == "help") {
+        helpCommand(arguments, receivedMessage)
+    } else if(primaryCommand == "murder"){
+        murderCommand(arguments, receivedMessage)
+    } else {
+        receivedMessage.channel.send("Invalid command")
+    }
+}
+
+function helpCommand(arguments, receivedMessage){
+    if(arguments.length == 0){
+        receivedMessage.channel.send("Let me show you all I can do")
+    }
+    else{
+        receivedMessage.channel.send("So you need help with" + arguments)
+    }
+}
+
+function murderCommand(arguments, receivedMessage){
+    if(arguments.length == 0){
+        receivedMessage.channel.send("Noone was chosen to murder")
+    }
+    else{
+        receivedMessage.channel.send("I can't murder him yet")
+    }
+}
+
+client.login("NjI1NzE1NzE0MTQxMTkyMjAy.XYjzeA.cz4uijgksQk8TFt_9vWAeKmmV0g")

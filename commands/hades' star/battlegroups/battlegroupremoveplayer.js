b7bc56941ba537b34eb7432abcb44cca76bbb093
Unknown <ivanlose@hotmail.com>
2019-11-05 12:48:28 -0300

    Removeplayer progress

---
+++ commands/hades' star/battlegroups/battlegroupremoveplayer.js
@@ -0,0 +1,54 @@
+let TechData = require("../../../Database/Hades' Star/techs.json")
+let { RichEmbed } = require("discord.js")
+let Player = require("../../../player.js")
+let Battlegroup = require("../../../battlegroup.js")
+
+module.exports = {
+    name: "battlegroupaddplayer",
+    category: "hades' star",
+    subcategory: "battlegroups",
+    description: "Add's a player to a white star battlegroup.",
+    usage: "&battlegroupaddplayer <battlegroupname> <role> <member>",
+    run: async (client, message, args) => {
+        let targetb
+        if(message.mentions.users > 1) return message.channel.send("You've mentioned more than one user.")
+        let user = message.mentions.users.first()
+        if(!user) return message.channel.send("You must specify who you are kicking!.")
+        else targetb = message.guild.member(user)
+
+        client.playersPrimeDB.ensure(`${targetb.id}`, Player.player(targetb, message))
+        client.playersRolePrimeDB.ensure(`${targetb.id}`, Battlegroup.battlegroupMember())
+
+        let battlegroup1 = client.battlegroups.get(`${message.guild.id}`, "battlegroup1.name")
+        let battlegroup2 = client.battlegroups.get(`${message.guild.id}`, "battlegroup2.name")
+        if(!battlegroup1) return message.channel.send("You have no battlegroups!")
+
+
+        const messagesplit = message.content.split(" ")
+        if(!messagesplit[1]) return message.channel.send("You must specify a battlegroup name!")
+        let knownbattlegroup
+        if(battlegroup1 === messagesplit[1]) {
+            knownbattlegroup = "battlegroup1"
+        }
+        else if(battlegroup2 === messagesplit[1]) {
+            knownbattlegroup = "battlegroup2"
+        }
+        else return message.channel.send("There's no battlegroup with this name in the corp!")
+
+        let authorrole = client.playersRolePrimeDB.get(`${message.author.id}`, `role${knownbattlegroup}`)
+        let authorrank = client.playersPrimeDB.get(`${message.author.id}`, "rank")
+        if(authorrank === "Officer" || authorrank ==="First Officer" || authorrole === "Captain"){}
+        else return message.channel.send("You must be at least an Officer or this group's Captain to remove someone from this battlegroup!")
+        
+        let battlegroupmembers = client.battlegroups.get(`${message.guild.id}`, `${knownbattlegroup}.members`)
+        if(battlegroupmembers.includes(targetb.id)){
+            let playerrole = client.playersRolePrimeDB.get(`${targetb.id}`, `role${knownbattlegroup}`)
+            if(playerrole.toLowerCase() === "captain"){
+                return message.channel.send("You cannot kick the captain of this battlegroup without setting another Captain first!")
+            }
+            
+
+
+        }
+    }
+}

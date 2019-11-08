07c456976df43cdd5df99e29639b55eae71a3018
Dragonflares <ivanlose@hotmail.com>
2019-11-07 23:01:43 -0300

    Adding specific battleship changes

--- commands/hades' star/ship/setplayerbattleship.js
+++ commands/hades' star/ship/setplayerbattleship.js
@@ -7,7 +7,7 @@ module.exports = {
     category: "hades' star",
     subcategory: "ship",
     description: "Sets the player's intended battleship for White Stars.",
-    usage: "&setplayerbattleship, then asnwer the bot's questions. Don't state any levels unless asked for.",
+    usage: "&setplayerbattleship, then asnwer the bot's questions. Don't state any levels unless asked for. You can also modify weapon, supports, and shield indiviually.",
     run: async (client, message, args) => {
         let targetb
         let user = message.mentions.users.first()
@@ -17,98 +17,160 @@ module.exports = {
         else return message.channel.send("You cannot set another player's battleship!")
 
         client.playerDB.ensure(`${targetb.id}`, Player.player(targetb, message))
-
-        
         let battleshipname
         let battleshiplevel
         let battleshipweapon
         let battleshipshield
         let battleshipsupport
-        message.channel.send("Please name your battleship")
-        try {
-            battleshipname = await message.channel.awaitMessages(message2 => message2.content.length < 16 , {
-                maxMatches: 1,
-                time: 40000,
-                errors: ['time', 'length']
-            });
-        } catch (err) {
-            console.error(err);
-            return message.channel.send("Invalid battleship name, check if it's larger than 15 characters.");
-        }
+        let messagesplitted = message.content.split(" ")
+        if(!messagesplitted[1]) {
 
-        message.channel.send("Please state your battleship's level")
-        try {
-            battleshiplevel = await message.channel.awaitMessages(message2 => message2.content < 7 && message2.content > 0 , {
-                maxMatches: 1,
-                time: 40000,
-                errors: ['time', 'level']
-            });
-        } catch (err) {
-            console.error(err);
-            return message.channel.send("Invalid battleship level.");
-        }
+            message.channel.send("Please name your battleship")
+            try {
+                battleshipname = await message.channel.awaitMessages(message2 => message2.content.length < 16 , {
+                    maxMatches: 1,
+                    time: 40000,
+                    errors: ['time', 'length']
+                });
+            } catch (err) {
+                console.error(err);
+                return message.channel.send("Invalid battleship name, check if it's larger than 15 characters.");
+            }
 
-        message.channel.send("Please state your battleship's weapon")
-        try {
-            battleshipweapon = await message.channel.awaitMessages(message2 => TechData[message2] && TechData[message2].Category === "Weapons", {
-                maxMatches: 1,
-                time: 40000,
-                errors: ['time', 'name']
-            });
-        } catch (err) {
-            console.error(err);
-            return message.channel.send("Invalid battleship weapon.");
-        }
+            message.channel.send("Please state your battleship's level")
+            try {
+                battleshiplevel = await message.channel.awaitMessages(message2 => message2.content < 7 && message2.content > 0 , {
+                    maxMatches: 1,
+                    time: 40000,
+                    errors: ['time', 'level']
+                });
+            } catch (err) {
+                console.error(err);
+                return message.channel.send("Invalid battleship level.");
+            }
 
-        message.channel.send("Please state your battleship's shield")
-        try {
-            battleshipshield = await message.channel.awaitMessages(message2 => TechData[message2] && TechData[message2].Category === "Shields", {
-                maxMatches: 1,
-                time: 40000,
-                errors: ['time', 'name']
-            });
-        } catch (err) {
-            console.error(err);
-            return message.channel.send("Invalid battleship shield.");
-        }
-       
-        client.playerDB.set(`${message.author.id}`, `${battleshipname.first().content}`, `battleship.name`)
+            message.channel.send("Please state your battleship's weapon")
+            try {
+                battleshipweapon = await message.channel.awaitMessages(message2 => TechData[message2] && TechData[message2].Category === "Weapons", {
+                    maxMatches: 1,
+                    time: 40000,
+                    errors: ['time', 'name']
+                });
+            } catch (err) {
+                console.error(err);
+                return message.channel.send("Invalid battleship weapon.");
+            }
 
-        client.playerDB.set(`${message.author.id}`, battleshiplevel.first().content , `battleship.level`)
+            message.channel.send("Please state your battleship's shield")
+            try {
+                battleshipshield = await message.channel.awaitMessages(message2 => TechData[message2] && TechData[message2].Category === "Shields", {
+                    maxMatches: 1,
+                    time: 40000,
+                    errors: ['time', 'name']
+                });
+            } catch (err) {
+                console.error(err);
+                return message.channel.send("Invalid battleship shield.");
+            }
         
-        let techlevelwep = await client.playerDB.get(`${message.author.id}`, `techs.${battleshipweapon.first().content}`)
-        if(techlevelwep == 0) return message.channel.send("You don't have this weapon researched!")
-        client.playerDB.set(`${message.author.id}`, `${battleshipweapon.first().content} ${techlevelwep}`, `battleship.weapon`)
- 
-        
-        let techlevelshield = await client.playerDB.get(`${message.author.id}`, `techs.${battleshipshield.first().content}`)
-        if(techlevelshield == 0) return message.channel.send("You don't have this shield researched!")
-        client.playerDB.set(`${message.author.id}`, `${battleshipshield.first().content} ${techlevelshield}`, `battleship.shield`)
-        client.playerDB.set(`${message.author.id}`, [], `battleship.support`)
-        if(parseInt(battleshiplevel.first().content) > 1) {
-            message.channel.send("Please state your battleship's support modules, pressing enter between each of them")
-            var i = 0
-            for(i ; i < parseInt(battleshiplevel.first().content) - 1 ; i++) {
+            client.playerDB.set(`${message.author.id}`, `${battleshipname.first().content}`, `battleship.name`)
+
+            client.playerDB.set(`${message.author.id}`, battleshiplevel.first().content , `battleship.level`)
+            
+            let techlevelwep = await client.playerDB.get(`${message.author.id}`, `techs.${battleshipweapon.first().content}`)
+            if(techlevelwep == 0) return message.channel.send("You don't have this weapon researched!")
+            client.playerDB.set(`${message.author.id}`, `${battleshipweapon.first().content} ${techlevelwep}`, `battleship.weapon`)
+    
+            
+            let techlevelshield = await client.playerDB.get(`${message.author.id}`, `techs.${battleshipshield.first().content}`)
+            if(techlevelshield == 0) return message.channel.send("You don't have this shield researched!")
+            client.playerDB.set(`${message.author.id}`, `${battleshipshield.first().content} ${techlevelshield}`, `battleship.shield`)
+            client.playerDB.set(`${message.author.id}`, [], `battleship.support`)
+            if(parseInt(battleshiplevel.first().content) > 1) {
+                message.channel.send("Please state your battleship's support modules, pressing enter between each of them")
+                var i = 0
+                for(i ; i < parseInt(battleshiplevel.first().content) - 1 ; i++) {
+                    try {
+                        battleshipsupport = await message.channel.awaitMessages(message2 => TechData[message2] && TechData[message2].Category === "Support", {
+                            maxMatches: 1,
+                            time: 40000,
+                            errors: ['time', 'name']
+                        });
+                    } catch (err) {
+                        console.error(err);
+                        return message.channel.send("Invalid battleship's support module.");
+                    }
+                    let techlevel = await client.playerDB.get(`${message.author.id}`, `techs.${battleshipsupport.first().content}`)
+                    if(techlevel == 0) return message.channel.send("You don't have this support module researched!")
+                    client.playerDB.push(`${message.author.id}`, `${battleshipsupport.first().content} ${techlevel}`, `battleship.support`)
+                }
+                return message.channel.send("Your battleship for white stars is now set.")
+            }
+            else {
+                return message.channel.send("Your battleship for white stars is now set.")
+            }
+        }
+        else {
+            if(!(client.playerDB.get(`${message.author.id}`, `battleship.name`))){
+                return message.channel.send("You don't have any battleship set!")
+            }
+            if(messagesplitted[1].toLowerCase() === "weapon" || messagesplitted[1].toLowerCase() === "w"){
+                message.channel.send("Please state your battleship's weapon after this message")
                 try {
-                    battleshipsupport = await message.channel.awaitMessages(message2 => TechData[message2] && TechData[message2].Category === "Support", {
+                    battleshipweapon = await message.channel.awaitMessages(message2 => TechData[message2] && TechData[message2].Category === "Weapons", {
                         maxMatches: 1,
                         time: 40000,
                         errors: ['time', 'name']
                     });
                 } catch (err) {
                     console.error(err);
-                    return message.channel.send("Invalid battleship's support module.");
+                    return message.channel.send("Invalid battleship weapon.");
                 }
-                let techlevel = await client.playerDB.get(`${message.author.id}`, `techs.${battleshipsupport.first().content}`)
-                if(techlevel == 0) return message.channel.send("You don't have this support module researched!")
-                client.playerDB.push(`${message.author.id}`, `${battleshipsupport.first().content} ${techlevel}`, `battleship.support`)
+                let techlevelwep = await client.playerDB.get(`${message.author.id}`, `techs.${battleshipweapon.first().content}`)
+                if(techlevelwep == 0) return message.channel.send("You don't have this weapon researched!")
+                client.playerDB.set(`${message.author.id}`, `${battleshipweapon.first().content} ${techlevelwep}`, `battleship.weapon`)
+            }
+            else if(messagesplitted[1].toLowerCase() === "shield" || messagesplitted[1].toLowerCase() === "sh"){
+                message.channel.send("Please state your battleship's shield after this message.")
+                try {
+                    battleshipshield = await message.channel.awaitMessages(message2 => TechData[message2] && TechData[message2].Category === "Shields", {
+                        maxMatches: 1,
+                        time: 40000,
+                        errors: ['time', 'name']
+                    });
+                } catch (err) {
+                    console.error(err);
+                    return message.channel.send("Invalid battleship shield.");
+                }
+                let techlevelshield = await client.playerDB.get(`${message.author.id}`, `techs.${battleshipshield.first().content}`)
+            if(techlevelshield == 0) return message.channel.send("You don't have this shield researched!")
+            client.playerDB.set(`${message.author.id}`, `${battleshipshield.first().content} ${techlevelshield}`, `battleship.shield`)
+            }
+            else if(messagesplitted[1].toLowerCase() === "support" || messagesplitted[1].toLowerCase() === "sp"){
+                battleshiplevel = client.playerDB.get(`${message.author.id}`, `battleship.level`)
+                if(battleshiplevel < 2) return message.channel.send("Your battleship is not level 2 yet, you cannot use Support modules!")
+                message.channel.send("Please state your battleship's support modules, pressing enter between each of them")
+                var i = 0
+                for(i ; i < battleshiplevel - 1 ; i++) {
+                    try {
+                        battleshipsupport = await message.channel.awaitMessages(message2 => TechData[message2] && TechData[message2].Category === "Support", {
+                            maxMatches: 1,
+                            time: 40000,
+                            errors: ['time', 'name']
+                        });
+                    } catch (err) {
+                        console.error(err);
+                        return message.channel.send("Invalid battleship's support module.");
+                    }
+                    let techlevel = await client.playerDB.get(`${message.author.id}`, `techs.${battleshipsupport.first().content}`)
+                    if(techlevel == 0) return message.channel.send("You don't have this support module researched!")
+                    client.playerDB.push(`${message.author.id}`, `${battleshipsupport.first().content} ${techlevel}`, `battleship.support`)
+                }
+            }
+            else {
+                return message.channel.send("Either invalid paramter to change, or you tried ot change the name or the level of the battleship. For that, the entire ship must be reset.")
             }
             return message.channel.send("Your battleship for white stars is now set.")
         }
-        else {
-            return message.channel.send("Your battleship for white stars is now set.")
-        }
-
-
     }
 }

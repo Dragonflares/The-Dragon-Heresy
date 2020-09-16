66c953977b762bc686208498d2c388a1f15ee36b
Choub <geoffrey.mayeux@gmail.com>
2020-09-16 19:42:42 +0200

    Remove old code

--- Models/TimeTracker.js
+++
@@ -1,9 +0,0 @@
-const {Schema, model} = require('mongoose')
-const Mongoose = require('mongoose')
-
-const TimeTracker = Schema ({
-    title: String,
-    time: Date
-})
-
-module.exports = model("TimeTracker", TimeTracker, "TimeTracker")

b8f1b4759e114aa2132d70257b87fe95bfee8631
Pencol <zinnalex@gmail.com>
2020-10-24 21:12:46 +0300

    Last Commit Added new RecruitWS system. Added Delete option.
    Forgot to mention new recruitws system on last commit.

--- plugins/HadesStarPlugin/commands/whitestars/RecruitWhiteStarCommand.js
+++ plugins/HadesStarPlugin/commands/whitestars/RecruitWhiteStarCommand.js
@@ -22,13 +22,13 @@ export class RecruitWhiteStarCommand extends WhitestarsCommand {
     else {
       if (!roles) return message.channel.send("Please input a discord role for the WS!")
       if (member.Corp.corpId === message.guild.id.toString())
-        return this.roleMessage(message, roles, args.join(' '))
+        return this.roleMessage(message, roles, args.join(' '),member)
       else
         return message.channel.send("You aren't on your Corporation's server!")
     }
   }
 
-  roleMessage = async (message, role, desc) => {
+  roleMessage = async (message, role, desc,member) => {
     message.delete({ timeout: 1 });    //Delete User message
 
     let description = 'Do you wish to partake in this White Star?';
@@ -36,52 +36,69 @@ export class RecruitWhiteStarCommand extends WhitestarsCommand {
 
     //Save Message ID
     //Get Whitestart with role
-    const ws = await WhiteStar.findOne({ wsrole: role.id }).exec()
+    const ws = await WhiteStar.findOne({ wsrole: role.id }).populate('author').populate('members').exec()
     if (!ws) {
-
-      let rolesEmbed = new Discord.MessageEmbed()
-        .setTitle(`White Star Recruitment by ${message.author.username}:`)
-        .setThumbnail("https://i.imgur.com/fNtJDNz.png")
-        .setDescription(`${description}`)
-        .addField("Group:", `<@&${role.id}>`)
-        .addField("Current People", "0")
-        .addField("Members", "None")
-        .setColor("ORANGE")
-        .setFooter(`⚔️ - Attack 🛡️ - Guardian 🗡️ - Assassin ❓ - Doesnt matter ❌ - Delist`)
-
-      const reactions = ['⚔️', '🛡️', '🗡️', '❓', '❌']
-
       //Send Message
-      const messageReaction = await message.channel.send(rolesEmbed);
-
-      //React
-      reactions.forEach(async react => await messageReaction.react(react))
+      let messageReaction = await this.sendWsMessage(message, role, description, 0, "None",member.name);
 
       //Create new Whitestar
       const corp = await Corp.findOne({ corpId: message.guild.id.toString() }).exec()
       let newWhiteStar = new WhiteStar({
         _id: new Mongoose.Types.ObjectId(),
-        author: message.author.username,
+        author: member,
         Corp: corp,
         wsrole: role.id,
         description: description,
         recruitmessage: messageReaction.id.toString(),
         retruitchannel: messageReaction.channel.id.toString(),
-        status: "Recruiting",   
+        status: "Recruiting",
         members: new Array(),
         preferences: new Map()
       })
       newWhiteStar.save();
     } else {
-      return message.channel.send("A WS is running on that group!")
+      if (ws.status == "Recruiting") {
+
+        let msg = await this.client.channels.cache.get(ws.retruitchannel).messages.fetch(ws.recruitmessage.toString())
+        msg.delete({ timeout: 1 })
+
+        //Get Members
+        let testString = ""
+        ws.members.forEach(t => testString += `${t.name} ${ws.preferences.get(t.discordId)}\n`)
+        if (testString == "") testString = "None";
+
+        //Send Message
+        let messageReaction = await this.sendWsMessage(message, role, ws.description, Object.keys(ws.members).length, testString,ws.author.name);
+
+        ws.recruitmessage = messageReaction.id.toString()
+        ws.retruitchannel = messageReaction.channel.id.toString()
+        ws.save()
+      } else {
+        return message.channel.send("A WS is running on that group!")
+      }
+
     }
-    //if null create one
+  }
 
-    //else
+  sendWsMessage = async (message, role, description, amount, members,author) => {
+    let rolesEmbed = new Discord.MessageEmbed()
+      .setTitle(`White Star Recruitment by ${author}:`)
+      .setThumbnail("https://i.imgur.com/fNtJDNz.png")
+      .setDescription(`${description}`)
+      .addField("Group:", `<@&${role.id}>`)
+      .addField("Current People", amount)
+      .addField("Members", members)
+      .setColor("ORANGE")
+      .setFooter(`⚔️ - Attack 🛡️ - Guardian 🗡️ - Assassin ❓ - Doesnt matter ❌ - Delist`)
 
-    //Check if there is a WS running with that role
-    //Resend message if on (recrut)
-    //else send message that RS is already running
+    const reactions = ['⚔️', '🛡️', '🗡️', '❓', '❌','🚮']
+
+    //Send Message
+    const messageReaction = await message.channel.send(rolesEmbed);
+
+    //React
+    reactions.forEach(async react => await messageReaction.react(react))
+    return messageReaction;
   }
 }
 

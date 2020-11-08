64d9d0961824f348cfd4ecf4ef79e663597d29b5
Pencol <zinnalex@gmail.com>
2020-11-08 20:56:06 +0200

    ConfirmTech bug fix

--- plugins/HadesStarPlugin/utils/confirmTech.js
+++ plugins/HadesStarPlugin/utils/confirmTech.js
@@ -1,4 +1,5 @@
 import { randomInt } from './randomInt';
+import { findBestMatch } from 'string-similarity';
 
 const replies = [
     "Allright, just retry without dyslexia.",
@@ -9,23 +10,23 @@ const replies = [
 ];
 
 export const confirmTech = async (message, query, tech) => {
-    if(tech.name.toLowerCase() != query.toLowerCase()){
+    if (tech.name.toLowerCase() != query.toLowerCase()) {
         message.channel.send(`Did you mean *${tech.name}* ?`);
         try {
             const response = await message.channel.awaitMessages(
-                m => m.author.id === message.author.id,{
+                m => m.author.id === message.author.id, {
                 max: 1,
                 time: 10000,
                 errors: ['time']
             });
 
             const possitiveList = ["y", "yes", "yeah", "yea", "yup", "yep", "ye", "sure", "absolutly", "of course", "right", "true"]
-            const rate = findBestMatch(response.first().content.toLowerCase(), Array.from(possitiveList));
+            const rate = await findBestMatch(response.first().content.toLowerCase(), Array.from(possitiveList));
             if (rate.bestMatch.rating < 0.5 && !possitiveList.some(v => response.first().content.toLowerCase().includes(v))) {
                 throw new Error();
             }
         } catch (err) {
-            message.channel.send(replies[randomInt(0, replies.length-1)]);
+            message.channel.send(replies[randomInt(0, replies.length - 1)]);
 
             return false;
         }

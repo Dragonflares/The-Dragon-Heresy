24ca0706de369d911b066797937676c67a63f907
Dragonflares <ivanlose@hotmail.com>
2021-05-15 14:01:31 -0300

    New Rank Role System

--- plugins/HadesStarPlugin/database/Models/Miner.js
+++ plugins/HadesStarPlugin/database/Models/Miner.js
@@ -2,8 +2,6 @@ import {Schema, model} from 'mongoose';
 
 const MinerSchema = Schema ({
     name: String,
-    level: {type: Number,
-        default: 0},
     support: {type: Schema.Types.ObjectId, ref: 'Tech'},
     mining: [
         {type: Schema.Types.ObjectId, ref: 'Tech'}

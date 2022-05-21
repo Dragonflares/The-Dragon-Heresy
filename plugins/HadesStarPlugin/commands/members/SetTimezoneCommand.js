261f1b18c8f50ba95af9894f33c6f83473b6d9b4
Pencol <zinnalex@gmail.com>
2022-05-21 20:11:07 +0300

    Fix: Plugin thinking the map is a command

--- plugins/HadesStarPlugin/commands/members/SetTimezoneCommand.js
+++ plugins/HadesStarPlugin/commands/members/SetTimezoneCommand.js
@@ -4,48 +4,6 @@ import { MemberDAO, CorpDAO } from '../../../../lib/utils/DatabaseObjects'
 import { MessageEmbed } from 'discord.js';
 const { MessageButton, MessageActionRow, MessageMenuOption, MessageMenu } = require("discord-buttons")
 
-export const timezones = new Map([
-    ["(GMT -12:00) Eniwetok, Kwajalein", "-12.00"],
-    ["(GMT -11:00) Midway Island, Samoa", "-11.00"],
-    ["(GMT -10:00) Hawaii", "-10.00"],
-    ["(GMT -9:30) Taiohae", "-09.50"],
-    ["(GMT -9:00) Alaska", "-09.00"],
-    ["(GMT -8:00) Pacific Time (US/Canada)", "-08.00"],
-    ["(GMT -7:00) Mountain Time (US/Canada)", "-07.00"],
-    ["(GMT -6:00) Central Time (US/Canada), Mexico City", "-06.00"],
-    ["(GMT -5:00) Eastern Time (US/Canada), Bogota, Lima", "-05.00"],
-    ["(GMT -4:30) Caracas", "-04.50"],
-    ["(GMT -4:00) Atlantic Time (Canada), Caracas, La Paz", "-04.00"],
-    ["(GMT -3:30) Newfoundland", "-03.50"],
-    ["(GMT -3:00) Brazil, Buenos Aires, Georgetown", "-03.00"],
-    ["(GMT -2:00) Mid-Atlantic", "-02.00"],
-    ["(GMT -1:00) Azores, Cape Verde Islands", "-01.00"],
-    ["(GMT) Western Europe Time, London, Lisbon, Casablanca", "+00.00"],
-    ["(GMT +1:00) Brussels, Copenhagen, Madrid, Paris", "+01.00"],
-    ["(GMT +2:00) Kaliningrad, South Africa, Jerusalem", "+02.00"],
-    ["(GMT +3:00) Baghdad, Riyadh, Moscow, St. Petersburg", "+03.00"],
-    ["(GMT +3:30) Tehran", "+03.50"],
-    ["(GMT +4:00) Abu Dhabi, Muscat, Baku, Tbilisi", "+04.00"],
-    ["(GMT +4:30) Kabul", "+04.50"],
-    ["(GMT +5:00) Ekaterinburg, Islamabad, Karachi, Tashkent", "+05.00"],
-    ["(GMT +5:30) Bombay, Calcutta, Madras, New Delhi", "+05.50"],
-    ["(GMT +5:45) Kathmandu, Pokhara", "+05.75"],
-    ["(GMT +6:00) Almaty, Dhaka, Colombo", "+06.00"],
-    ["(GMT +6:30) Yangon, Mandalay", "+06.50"],
-    ["(GMT +7:00) Bangkok, Hanoi, Jakarta", "+07.00"],
-    ["(GMT +8:00) Beijing, Perth, Singapore, Hong Kong", "+08.00"],
-    ["(GMT +8:45) Eucla", "+08.75"],
-    ["(GMT +9:00) Tokyo, Seoul, Osaka, Sapporo, Yakutsk", "+09.00"],
-    ["(GMT +9:30) Adelaide, Darwin", "+09.50"],
-    ["(GMT +10:00) Eastern Australia, Guam, Vladivostok", "+10.00"],
-    ["(GMT +10:30) Lord Howe Island", "+10.50"],
-    ["(GMT +11:00) Magadan, Solomon Islands, New Caledonia", "+11.00"],
-    ["(GMT +11:30) Norfolk Island", "+11.50"],
-    ["(GMT +12:00) Auckland, Wellington, Fiji, Kamchatka", "+12.00"],
-    ["(GMT +12:45) Chatham Islands", "+12.75"],
-    ["(GMT +13:00) Apia, Nukualofa", "+13.00"],
-    ["(GMT +14:00) Line Islands, Tokelau", "+14.00"]
-])
 
 export class SetTimezoneCommand extends MemberCommand{
     constructor(plugin){
@@ -70,7 +28,50 @@ export class SetTimezoneCommand extends MemberCommand{
         let member = await Member.findOne({ discordId: target.id.toString() }).populate('Corp').populate('techs').populate('reminders').exec();
         if (!member)
             return message.channel.send("You aren't part of any Corporation. Join a Corporation first.")
-
+        //Timezones
+        
+        const timezones = new Map([
+            ["(GMT -12:00) Eniwetok, Kwajalein", "-12.00"],
+            ["(GMT -11:00) Midway Island, Samoa", "-11.00"],
+            ["(GMT -10:00) Hawaii", "-10.00"],
+            ["(GMT -9:30) Taiohae", "-09.50"],
+            ["(GMT -9:00) Alaska", "-09.00"],
+            ["(GMT -8:00) Pacific Time (US/Canada)", "-08.00"],
+            ["(GMT -7:00) Mountain Time (US/Canada)", "-07.00"],
+            ["(GMT -6:00) Central Time (US/Canada), Mexico City", "-06.00"],
+            ["(GMT -5:00) Eastern Time (US/Canada), Bogota, Lima", "-05.00"],
+            ["(GMT -4:30) Caracas", "-04.50"],
+            ["(GMT -4:00) Atlantic Time (Canada), Caracas, La Paz", "-04.00"],
+            ["(GMT -3:30) Newfoundland", "-03.50"],
+            ["(GMT -3:00) Brazil, Buenos Aires, Georgetown", "-03.00"],
+            ["(GMT -2:00) Mid-Atlantic", "-02.00"],
+            ["(GMT -1:00) Azores, Cape Verde Islands", "-01.00"],
+            ["(GMT) Western Europe Time, London, Lisbon, Casablanca", "+00.00"],
+            ["(GMT +1:00) Brussels, Copenhagen, Madrid, Paris", "+01.00"],
+            ["(GMT +2:00) Kaliningrad, South Africa, Jerusalem", "+02.00"],
+            ["(GMT +3:00) Baghdad, Riyadh, Moscow, St. Petersburg", "+03.00"],
+            ["(GMT +3:30) Tehran", "+03.50"],
+            ["(GMT +4:00) Abu Dhabi, Muscat, Baku, Tbilisi", "+04.00"],
+            ["(GMT +4:30) Kabul", "+04.50"],
+            ["(GMT +5:00) Ekaterinburg, Islamabad, Karachi, Tashkent", "+05.00"],
+            ["(GMT +5:30) Bombay, Calcutta, Madras, New Delhi", "+05.50"],
+            ["(GMT +5:45) Kathmandu, Pokhara", "+05.75"],
+            ["(GMT +6:00) Almaty, Dhaka, Colombo", "+06.00"],
+            ["(GMT +6:30) Yangon, Mandalay", "+06.50"],
+            ["(GMT +7:00) Bangkok, Hanoi, Jakarta", "+07.00"],
+            ["(GMT +8:00) Beijing, Perth, Singapore, Hong Kong", "+08.00"],
+            ["(GMT +8:45) Eucla", "+08.75"],
+            ["(GMT +9:00) Tokyo, Seoul, Osaka, Sapporo, Yakutsk", "+09.00"],
+            ["(GMT +9:30) Adelaide, Darwin", "+09.50"],
+            ["(GMT +10:00) Eastern Australia, Guam, Vladivostok", "+10.00"],
+            ["(GMT +10:30) Lord Howe Island", "+10.50"],
+            ["(GMT +11:00) Magadan, Solomon Islands, New Caledonia", "+11.00"],
+            ["(GMT +11:30) Norfolk Island", "+11.50"],
+            ["(GMT +12:00) Auckland, Wellington, Fiji, Kamchatka", "+12.00"],
+            ["(GMT +12:45) Chatham Islands", "+12.75"],
+            ["(GMT +13:00) Apia, Nukualofa", "+13.00"],
+            ["(GMT +14:00) Line Islands, Tokelau", "+14.00"]
+        ])
 
         //Create message
         let embed = await this.SendMessage(member.timezone)

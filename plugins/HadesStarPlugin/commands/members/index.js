81c301ae7eb43f4a7abaa3498eb9930ae5d6b7ce
Pencol <zinnalex@gmail.com>
2022-06-10 11:38:58 +0300

    Initial new RSLOG

--- plugins/HadesStarPlugin/commands/members/index.js
+++ plugins/HadesStarPlugin/commands/members/index.js
@@ -16,4 +16,3 @@ export * from './RemindListCommand';
 export * from './RemindRemoveCommand';
 export * from './SetTimezoneCommand';
 export * from './TestCommand';
-export * from './NewRedStarLogCommand';
